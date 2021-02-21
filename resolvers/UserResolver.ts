3588617295ff1b15710adedddc496f21e2b7802f
mikohan <angara99@gmail.com>
2021-02-21 15:19:02 +0300

    Start authentication

---
+++ resolvers/UserResolver.ts
@@ -0,0 +1,21 @@
+import { Resolver, Query, UseMiddleware, Arg, Ctx } from 'type-graphql';
+
+import { ObjectId } from 'mongodb';
+import { MyContext } from '../types/MyContext';
+import { isAuth } from '../types/isAuth';
+import { User, UserModel } from '../entity/User';
+import { ObjectIdScalar } from '../schema/object-id.scalar';
+
+@Resolver(() => User)
+export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async user(@Arg('userId', () => ObjectIdScalar) userId: ObjectId) {
+    return await UserModel.findById(userId);
+  }
+
+  @Query(() => User, { nullable: true })
+  @UseMiddleware(isAuth)
+  async currentUser(@Ctx() ctx: MyContext): Promise<User | null> {
+    return await UserModel.findByid(ctx.res.locals.userId);
+  }
+}
