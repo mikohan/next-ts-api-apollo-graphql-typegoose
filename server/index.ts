b62f546d98abab1c2205bd62c8f9a6e5b05692e9
mikohan <angara99@gmail.com>
2021-02-21 18:09:02 +0300

    Server initialized

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import cors from 'cors';
 
-import createShcema from '../schema';
+import createSchema from '../schema';
 import createSession from '../session';
 
 const port = process.env.PORT || 8888;
@@ -15,7 +15,41 @@ async function createServer() {
     await createSession();
     // 2. Create express server
     const app = express();
+
+    const corsOptions = {
+      origin: process.env.URL_APP,
+      credentials: true,
+    };
+
+    app.use(cors(corsOptions));
+
+    // use JSON requests
+    app.use(express.json());
+    const schema = await createSchema();
+    // create GraphQLServer
+    const apolloServer = new ApolloServer({
+      schema,
+      context: ({ req, res }) => ({ req, res }),
+      introspection: true,
+      //enable playground
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+    });
+
+    apolloServer.applyMiddleware({ app, cors: corsOptions });
+
+    // start server
+    app.listen({ port }, () => {
+      console.log(
+        `Server is running at http://localhost:${port}${apolloServer.graphqlPath}`
+      );
+    });
   } catch (e) {
     console.error(e);
   }
 }
+
+createServer();
